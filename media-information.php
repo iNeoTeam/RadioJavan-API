e929dbdc01dcbdb75ce964e5d13362e849df201f
iNeoTeam | آی نئو <58592346+iNeoTeam@users.noreply.github.com>
2021-12-31 19:32:16 +0330

    Update media-information.php

--- media-information.php
+++ media-information.php
@@ -5,19 +5,19 @@ $api = "https://api.ineo-team.ir"; //don't change it.
 $parameters = array(
    'auth' => "rj2995170299", // http-access-token
    'action' => "idfinder", // request method
-   'link' => "https://www.radiojavan.com/mp3s/mp3/Hamid-Sefat-Too-Deli" // radiojavan music/video link
+   'link' => "https://www.radiojavan.com/mp3s/mp3/Hamid-Sefat-Too-Deli" // radiojavan music/video/podcast link
 );
 $output = json_decode(file_get_contents($api."/radiojavan.php?".http_build_query($parameters)));
 if($output->status == "successfully"){
     $id = $output->result->id;
     $type = $output->result->type;
-    if(!in_array($type, ['music', 'video'])){
+    if(!in_array($type, ['music', 'video', 'podcast'])){
         echo json_encode(['ok' => false, 'status' => "unknown type"]);
         exit();
     }
     $param = array(
         'auth' => "rj2995170299",
-        'action' => $type, //music or video 
+        'action' => $type, //music or video or podcast
         'id' => $id
     );
     $out = json_decode(file_get_contents($api."/radiojavan.php?".http_build_query($param)));
