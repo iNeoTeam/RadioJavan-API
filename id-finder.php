872b21fd9e59f66a93557ae8554d799cfcbd7249
iNeoTeam | آی نئو <58592346+iNeoTeam@users.noreply.github.com>
2022-01-04 15:39:52 +0330

    Update id-finder.php

--- id-finder.php
+++ id-finder.php
@@ -5,7 +5,7 @@ $api = "https://api.ineo-team.ir"; //don't change it.
 $parameters = array(
    'auth' => "rj2995170299", // http-access-token
    'action' => "idfinder", // request method
-   'link' => "https://www.radiojavan.com/videos/video/hamid-sefat-bakhshesh-(ft-amirabbas-golab)" // radiojavan music/video link
+   'link' => "https://www.radiojavan.com/videos/video/hamid-sefat-bakhshesh-(ft-amirabbas-golab)" // radiojavan music/video/podcast link
 );
 $output = json_decode(file_get_contents($api."/radiojavan.php?".http_build_query($parameters)));
 if($output->status == "successfully"){
