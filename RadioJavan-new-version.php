69be7b35fb7bea8ae83100b57d762b19cd2bcc6e
iNeoTeam | آی نئو <58592346+iNeoTeam@users.noreply.github.com>
2022-01-07 14:47:51 +0330

    RadioJavan Full Access API

--- RadioJavan-new-version.php
+++ RadioJavan-new-version.php
@@ -1,14 +1,23 @@
 <?php
 /*
-	* Name:				RadioJavan Full Access API
-	* Language:			PHP
-	* Documents:		https://api.ineo-team.ir/docs.php?apiName=radiojavan
-	* Examples:			https://github.com/iNeoTeam/RadioJavan-API
-	* Telegram Bot:		@RadioJavanComBot
-	* Get HTTP TOKEN:	T.me/RadioJavanComBot?start=dev
-	* Coded By:			Sir.4m1R [Contact: T.me/CrossXss]
-	* Powered By:		iNeoTeam [Contact: T.me/iNeoTeam]
-	* Date:				1400/10/17 | 13:00:00 (Asia/Tehran)
+	* Name:
+		RadioJavan Full Access API
+	* Language:
+		PHP
+	* Documents:
+		https://api.ineo-team.ir/docs.php?apiName=radiojavan
+	* Examples:
+		https://github.com/iNeoTeam/RadioJavan-API
+	* Telegram Bot:
+		@RadioJavanComBot
+	* Get HTTP TOKEN:
+		T.me/RadioJavanComBot?start=dev
+	* Coded By:
+		Sir.4m1R [Contact: T.me/CrossXss]
+	* Powered By:
+		iNeoTeam [Contact: T.me/iNeoTeam]
+	* Date:
+		1400/10/17 | 13:00:00 (Asia/Tehran)
 */
 error_reporting(0);
 header("content-type: application/json; charset= UTF-8");
