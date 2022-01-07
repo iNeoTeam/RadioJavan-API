5fbffbe26d1963e9af59330131556952d72b8861
iNeoTeam | آی نئو <58592346+iNeoTeam@users.noreply.github.com>
2022-01-07 11:24:05 +0330

    update-v3

--- get-new-media.php
+++ get-new-media.php
@@ -5,7 +5,7 @@ $api = "https://api.ineo-team.ir"; //don't change it.
 $parameters = array(
    'auth' => "rj9143484418", // http-access-token
    'action' => "new", // request method
-   'type' => "music", // music/video
+   'type' => "music", // music/video/podcast/album
 );
 $output = json_decode(file_get_contents($api."/radiojavan.php?".http_build_query($parameters)));
 if($output->status == "successfully"){
